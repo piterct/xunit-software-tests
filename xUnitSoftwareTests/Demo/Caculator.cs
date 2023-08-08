2707a8f3f3b1092d5bb1247114f26a4c574167cb
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-08 00:03:54 -0300

    Caculator class

---
+++ xUnitSoftwareTests/Demo/Caculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo
+{
+    public class Caculator
+    {
+        public double Sum(double v1, double v2)
+        {
+            return v1 + v2;
+        }
+
+        public int Divide(int v1, int v2)
+        {
+            return v1 / v2;
+        }
+    }
+}
