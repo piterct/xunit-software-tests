6039e59d78686ad8ff4c5ea979e04c7bcbaeeccc
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-08 23:57:50 -0300

    Fact Calculator_Sum_MustBeEqual

---
+++ xUnitSoftwareTests/Demo.Tests/03 - AssertNumbers.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertNumbers
+    {
+        [Fact]
+        public void Calculator_Sum_MustBeEqual()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Sum(1, 2);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+    }
+}
