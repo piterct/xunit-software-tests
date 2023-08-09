0eec96e0481ad8b5990d6c210c398ce4900a5e77
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-09 00:06:27 -0300

    Assert - Employee_Name_MustNotBeNullOrEmpty

---
+++ xUnitSoftwareTests/Demo.Tests/04 - AssertNullBoolTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertNullBoolTests
+    {
+        [Fact]
+        public void Employee_Name_MustNotBeNullOrEmpty()
+        {
+            // Arrange & Act
+            var employee = new Employee("", 1000);
+
+            // Assert
+            Assert.False(string.IsNullOrEmpty(employee.Name));
+        }
+    }
+}
