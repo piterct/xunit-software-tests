e8848658861f23bb88283782dbca3ca5851bc80f
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-08 23:59:20 -0300

    Assert Number - Calculator_Sum_MustBeNotEqual

--- xUnitSoftwareTests/Demo.Tests/03 - AssertNumbers.cs
+++ xUnitSoftwareTests/Demo.Tests/03 - AssertNumbers.cs
@@ -16,5 +16,18 @@ namespace Demo.Tests
             // Assert
             Assert.Equal(3, result);
         }
+
+        [Fact]
+        public void Calculator_Sum_MustBeNotEqual()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Somar(1.13123123123, 2.2312313123);
+
+            // Assert
+            Assert.NotEqual(3.3, result, 1);
+        }
     }
 }
