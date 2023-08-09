afa27ffa362a9d494b8a7fd72ea03f5f7f310b00
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-09 00:08:42 -0300

    Employee_NickName_NotHasNickName

--- xUnitSoftwareTests/Demo.Tests/04 - AssertNullBoolTests.cs
+++ xUnitSoftwareTests/Demo.Tests/04 - AssertNullBoolTests.cs
@@ -13,5 +13,19 @@ namespace Demo.Tests
             // Assert
             Assert.False(string.IsNullOrEmpty(employee.Name));
         }
+
+        [Fact]
+        public void Employee_NickName_NotHasNickName()
+        {
+            // Arrange & Act
+            var employee = new Employee("Michael", 1000);
+
+            // Assert
+            Assert.Null(employee.NickName);
+
+            // Assert Bool
+            Assert.True(string.IsNullOrEmpty(employee.NickName));
+            Assert.False(employee.NickName?.Length > 0);
+        }
     }
 }
