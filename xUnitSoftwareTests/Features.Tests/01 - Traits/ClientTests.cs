3a940457bd46c94f26a79ba4c17a53c396c2f37b
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-10 20:56:30 -0300

    Traits Clients

---
+++ xUnitSoftwareTests/Features.Tests/01 - Traits/ClientTests.cs
@@ -0,0 +1,31 @@
+﻿using Features.Clients;
+using Xunit;
+
+namespace Features.Tests._01___Traits
+{
+    public class ClientTests
+    {
+
+        [Fact(DisplayName = "New Client is valid")]
+        [Trait("Category", "Client Trait Tests")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = new Client(
+                Guid.NewGuid(),
+                "Michael",
+                "Peter",
+                DateTime.Now.AddYears(-30),
+                "michael@edu.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.IsValid();
+
+            // Assert 
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
