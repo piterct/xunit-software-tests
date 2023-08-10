08193bea6a4251dbca2ca80f4924845aafa2a9fd
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-10 19:45:48 -0300

    EmployeeFactory class

---
+++ xUnitSoftwareTests/Demo/EmployeeFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class EmployeeFactory
+    {
+        public static Employee Create(string name, double salary)
+        {
+            return new Employee(name, salary);
+        }
+    }
+}
