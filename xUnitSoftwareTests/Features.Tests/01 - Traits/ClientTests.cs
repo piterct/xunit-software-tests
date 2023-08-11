279add0016989d84706708631e216941bab87ab6
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-10 21:03:05 -0300

    Traits - Client -  Client_NewClient_MustBeInValid

--- xUnitSoftwareTests/Features.Tests/01 - Traits/ClientTests.cs
+++ xUnitSoftwareTests/Features.Tests/01 - Traits/ClientTests.cs
@@ -11,7 +11,7 @@ namespace Features.Tests._01___Traits
         public void Client_NewClient_MustBeValid()
         {
             // Arrange
-            var cliente = new Client(
+            var client = new Client(
                 Guid.NewGuid(),
                 "Michael",
                 "Peter",
@@ -21,11 +21,33 @@ namespace Features.Tests._01___Traits
                 DateTime.Now);
 
             // Act
-            var result = cliente.IsValid();
+            var result = client.IsValid();
 
             // Assert 
             Assert.True(result);
-            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+            Assert.Equal(0, client.ValidationResult.Errors.Count);
+        }
+
+        [Fact(DisplayName = "New Client is valid")]
+        [Trait("Category", "Client Trait Tests")]
+        public void Client_NewClient_MustBeInValid()
+        {
+            // Arrange
+            var client = new Client(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "edu2edu.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = client.IsValid();
+
+            // Assert 
+            Assert.False(result);
+            Assert.NotEqual(0, client.ValidationResult.Errors.Count);
         }
     }
 }
