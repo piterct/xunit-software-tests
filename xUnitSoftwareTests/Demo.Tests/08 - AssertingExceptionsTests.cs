f3f1aafb198e37b784d1cbb47a6a3d26be53a438
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-12 12:47:52 -0300

    asserting exceptions tests

---
+++ xUnitSoftwareTests/Demo.Tests/08 - AssertingExceptionsTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertingExceptionsTests
+    {
+        [Fact]
+        public void Calculator_Divide_MustReturnErrorDivideByZero()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act & Assert
+            Assert.Throws<DivideByZeroException>(() => calculator.Divide(10, 0));
+        }
+
+        [Fact]
+        public void Employee_Salary_MustReturnErrorSalaryLessThanAllowed()
+        {
+            // Arrange & Act & Assert
+            var exception =
+                Assert.Throws<Exception>(() => EmployeeFactory.Create("Michael", 250));
+
+            Assert.Equal("Salary less than allowed", exception.Message);
+        }
+    }
+}
