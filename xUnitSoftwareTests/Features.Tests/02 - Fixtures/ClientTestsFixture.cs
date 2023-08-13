6247e3410b6096438ccbe4e12ae9684685dc91d2
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-12 23:52:00 -0300

    Example to use ClientFixture with many cases of clients

---
+++ xUnitSoftwareTests/Features.Tests/02 - Fixtures/ClientTestsFixture.cs
@@ -0,0 +1,41 @@
+﻿using Features.Clients;
+
+namespace Features.Tests._02___Fixtures
+{
+    public class ClientTestsFixture : IDisposable
+    {
+
+        public Client GenerateValidClient()
+        {
+            var client = new Client(
+           Guid.NewGuid(),
+           "Michael",
+           "Peter",
+           DateTime.Now.AddYears(-30),
+           "michael@edu.com",
+           true,
+           DateTime.Now);
+
+            return client;
+        }
+
+        public Client GenerateInvalidClient()
+        {
+            var client = new Client(
+             Guid.NewGuid(),
+             "",
+             "",
+             DateTime.Now,
+             "michael@edu.com",
+             true,
+             DateTime.Now);
+
+            return client;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
