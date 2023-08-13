7ca942ed9bd410a9eef2a4d685405d19bb8aea5a
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-13 20:31:33 -0300

    Method Test01 to priority tests using the class ordertests

--- xUnitSoftwareTests/Features.Tests/03 - Order/OrderTests.cs
+++ xUnitSoftwareTests/Features.Tests/03 - Order/OrderTests.cs
@@ -19,5 +19,16 @@ namespace Features.Tests._03___Order
             Assert.True(Test1Called);
             Assert.True(Test2Called);
         }
+
+        [Fact(DisplayName = "Test 01")]
+        [Trait("Category", "Order Tests")]
+        public void Test01()
+        {
+            Test4Called = true;
+
+            Assert.True(Test3Called);
+            Assert.True(Test1Called);
+            Assert.True(Test2Called);
+        }
     }
 }
