6baab04a6e3b2b73ceb9eb212af68bda008f4677
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-13 20:21:47 -0300

    Properties tests called

--- xUnitSoftwareTests/Features.Tests/03 - Order/OrderTests.cs
+++ xUnitSoftwareTests/Features.Tests/03 - Order/OrderTests.cs
@@ -2,5 +2,9 @@
 {
     public class OrderTests
     {
+        public static bool Test1Called;
+        public static bool Test2Called;
+        public static bool Test3Called;
+        public static bool Test4Called;
     }
 }
