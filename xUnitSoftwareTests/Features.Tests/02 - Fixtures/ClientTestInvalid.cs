871e7f5e421e23cff5191f97b8385ce74cb0f1d9
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-13 00:30:10 -0300

    ClientTestInvalid fixture

--- xUnitSoftwareTests/Features.Tests/02 - Fixtures/ClientTestInvalid.cs
+++ xUnitSoftwareTests/Features.Tests/02 - Fixtures/ClientTestInvalid.cs
@@ -1,6 +1,30 @@
-﻿namespace Features.Tests._02___Fixtures
+﻿using Xunit;
+
+namespace Features.Tests._02___Fixtures
 {
+    [Collection(nameof(ClientCollection))]
     public class ClientTestInvalid
     {
+        private readonly ClientTestsFixture _clientTestsFixture;
+
+        public ClientTestInvalid(ClientTestsFixture clientTestsFixture)
+        {
+            _clientTestsFixture = clientTestsFixture;
+        }
+
+        [Fact(DisplayName = "New invalid client")]
+        [Trait("Category", "Client Fixture Tests")]
+        public void Client_NewClient_MustBeInValid()
+        {
+            // Arrange
+            var client = _clientTestsFixture.GenerateInvalidClient();
+
+            // Act
+            var result = client.IsValid();
+
+            // Assert 
+            Assert.False(result);
+            Assert.NotEqual(0, client.ValidationResult.Errors.Count);
+        }
     }
 }
