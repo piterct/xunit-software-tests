57ad2d3be9ef3f612e26eedf0e5bd8850db949f8
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-13 00:38:48 -0300

    Asserting object type tests

---
+++ xUnitSoftwareTests/Demo.Tests/06 - AssertingObjectTypesTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertingObjectTypesTests
+    {
+        [Fact]
+        public void EmployeeFactory_Create_MustReturnEmployeeType()
+        {
+            // Arrange & Act
+            var employee = EmployeeFactory.Create("Michael", 10000);
+
+            // Assert
+            Assert.IsType<Employee>(employee);
+        }
+
+        [Fact]
+        public void EmployeeFactory_Create_MustReturnEmployeeFromPerson()
+        {
+            // Arrange & Act
+            var employee = EmployeeFactory.Create("Michael", 10000);
+
+            // Assert
+            Assert.IsAssignableFrom<Person>(employee);
+        }
+    }
+}
