6b570e5db97f6431dc7f8ae3638519f6d347cae5
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-13 00:24:20 -0300

    ClientTestValid using fixture class to useable methods from valid client data.

---
+++ xUnitSoftwareTests/Features.Tests/02 - Fixtures/ClientTestValid.cs
@@ -0,0 +1,31 @@
+﻿using Features.Clients;
+using Xunit;
+
+namespace Features.Tests._02___Fixtures
+{
+    public class ClientTestValid
+    {
+
+        private readonly ClientTestsFixture _clientTestsFixture;
+
+        public ClientTestValid(ClientTestsFixture clientTestsFixture)
+        {
+            _clientTestsFixture = clientTestsFixture;
+        }
+
+        [Fact(DisplayName = "New valid client")]
+        [Trait("Category", "Client Fixture Tests")]
+        public void Client_NewClient_MustBeValid()
+        {
+            // Arrange
+            var client = _clientTestsFixture.GenerateValidClient();
+
+            // Act
+            var result = client.IsValid();
+
+            // Assert 
+            Assert.True(result);
+            Assert.Equal(0, client.ValidationResult.Errors.Count);
+        }
+    }
+}
