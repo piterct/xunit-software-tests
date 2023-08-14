a210202620aafc7f31275d9789583ad6c00e5336
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-14 00:26:29 -0300

    Asserting Colletions tests

---
+++ xUnitSoftwareTests/Demo.Tests/07 - AssertingColletionsTests.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertingColletionsTests
+    {
+        [Fact]
+        public void Employee_Skills_MustNotHasEmptySkills()
+        {
+            // Arrange & Act
+            var employe = EmployeeFactory.Create("Michael", 10000);
+
+            // Assert
+            Assert.All(employe.Skills, skill => Assert.False(string.IsNullOrWhiteSpace(skill)));
+        }
+
+        [Fact]
+        public void Employee_Skills_JuniorMustHasBasicSkill()
+        {
+            // Arrange & Act
+            var employe = EmployeeFactory.Create("Michael", 1000);
+
+            // Assert
+            Assert.Contains("OOP", employe.Skills);
+        }
+
+        [Fact]
+        public void Employee_Skills_JuniorMustNotHasAdvancedSkill()
+        {
+            // Arrange & Act
+            var employe = EmployeeFactory.Create("Michael", 1000);
+
+            // Assert
+            Assert.DoesNotContain("Microservices", employe.Skills);
+        }
+
+        [Fact]
+        public void Employee_Skills_SeniorMustHasAllSkills()
+        {
+            // Arrange & Act
+            var employe = EmployeeFactory.Create("Michael", 15000);
+
+            var basicSkills = new[]
+            {
+                "Programming logic",
+                "OOP",
+                "Tests",
+                "Microservices"
+            };
+
+            // Assert
+            Assert.Equal(basicSkills, employe.Skills);
+        }
+    }
+}
