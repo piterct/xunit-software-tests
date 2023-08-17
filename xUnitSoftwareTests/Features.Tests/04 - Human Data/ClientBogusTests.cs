d988d89eb023a6396a3d8954f4d8f0aa6b0e6e65
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-17 01:19:39 -0300

    ClientBogusTests with ClientBogusTestsFixture

--- xUnitSoftwareTests/Features.Tests/04 - Human Data/ClientBogusTests.cs
+++ xUnitSoftwareTests/Features.Tests/04 - Human Data/ClientBogusTests.cs
@@ -1,6 +1,30 @@
-﻿namespace Features.Tests
+﻿using Features.Tests._04___Human_Data;
+using Xunit;
+
+namespace Features.Tests
 {
     public class ClientBogusTests
     {
+        private readonly ClientBogusTestsFixture _clientBogusTestsFixture;
+
+        public ClientBogusTests(ClientBogusTestsFixture clientBogusTestsFixture)
+        {
+            _clientBogusTestsFixture = clientBogusTestsFixture;
+        }
+
+        [Fact(DisplayName = "New Valid Client")]
+        [Trait("Category", "Client Bogus Tests")]
+        public void Client_NewClient_MustBeValid()
+        {
+            // Arrange
+            var client = _clientBogusTestsFixture.GenerateValidNewClient();
+
+            // Act
+            var result = client.IsValid();
+
+            // Assert 
+            Assert.True(result);
+            Assert.Equal(0, client.ValidationResult.Errors.Count);
+        }
     }
 }
