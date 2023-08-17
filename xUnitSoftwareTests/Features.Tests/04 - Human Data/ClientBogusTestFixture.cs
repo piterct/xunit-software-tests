a5facbe06db73e68e1c875a27c7863b13e89f8d5
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-16 21:05:30 -0300

    cleaning using on the clientBogusTestsFixture

--- xUnitSoftwareTests/Features.Tests/04 - Human Data/ClientBogusTestFixture.cs
+++ xUnitSoftwareTests/Features.Tests/04 - Human Data/ClientBogusTestFixture.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Features.Tests._04___Human_Data
+﻿namespace Features.Tests._04___Human_Data
 {
-    internal class ClientBogusTestFixture
+    public  class ClientBogusTestFixture
     {
     }
 }
