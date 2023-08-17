819ffe774cc114613e3b8f4ae1def05493261db0
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-17 01:10:27 -0300

    ClientBogusTestsFixture to the  valid new client

--- xUnitSoftwareTests/Features.Tests/04 - Human Data/ClientBogusTestsFixture.cs
+++ xUnitSoftwareTests/Features.Tests/04 - Human Data/ClientBogusTestsFixture.cs
@@ -1,6 +1,30 @@
-﻿namespace Features.Tests._04___Human_Data
+﻿using Bogus;
+using Bogus.DataSets;
+using Features.Clients;
+
+namespace Features.Tests._04___Human_Data
 {
     public  class ClientBogusTestsFixture
     {
+        public Client GenerateValidNewClient()
+        {
+
+            var gender = new Faker().PickRandom<Name.Gender>();
+
+            var client = new Faker<Client>("pt_BR")
+                .CustomInstantiator(f => new Client(
+                    Guid.NewGuid(),
+                    f.Name.FirstName(gender),
+                    f.Name.LastName(gender),
+                    f.Date.Past(80, DateTime.Now.AddDays(-18)),
+                    "",
+                    true,
+                    DateTime.Now))
+                .RuleFor(c => c.Email, (f, c) =>
+                f.Internet.Email(c.Name.ToLower(), c.LasName.ToLower()));
+
+
+            return client;
+        }
     }
 }
