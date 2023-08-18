f1d1edc56c903ad1dd1101371150520a12848f01
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-18 08:16:38 -0300

    ClientBogusTests class

--- xUnitSoftwareTests/Features.Tests/04 - Human Data/ClientBogusTests.cs
+++ xUnitSoftwareTests/Features.Tests/04 - Human Data/ClientBogusTests.cs
@@ -26,5 +26,20 @@ namespace Features.Tests
             Assert.True(result);
             Assert.Equal(0, client.ValidationResult.Errors.Count);
         }
+
+        [Fact(DisplayName = "New Invalid Client")]
+        [Trait("Category", "Client Bogus Tests")]
+        public void Client_NewClient_MustBeInvalid()
+        {
+            // Arrange
+            var client = _clientBogusTestsFixture.GenerateInvalidClient();
+
+            // Act
+            var result = client.IsValid();
+
+            // Assert 
+            Assert.False(result);
+            Assert.NotEqual(0, client.ValidationResult.Errors.Count);
+        }
     }
 }
