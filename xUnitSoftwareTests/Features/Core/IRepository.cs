e9836ce70184f4c3450db66d5ce1248abc31cc3c
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-22 22:01:15 -0300

    IRepository Interface

---
+++ xUnitSoftwareTests/Features/Core/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Features.Core
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        void Add(TEntity obj);
+        TEntity GetById(Guid id);
+        IEnumerable<TEntity> GetAll();
+        void Update(TEntity obj);
+        void Remove(Guid id);
+        IEnumerable<TEntity> Search(Expression<Func<TEntity, bool>> predicate);
+        int Commit();
+    }
+}
