3f91f6c1e01a092ded2255a80f581d5cfc1edbe9
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-22 22:18:48 -0300

    ClientEmailNotification

---
+++ xUnitSoftwareTests/Features/Clients/ClientEmailNotification.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Features.Clients
+{
+    public class ClientEmailNotification : INotification
+    {
+        public string Origin { get; private set; }
+        public string Destination { get; private set; }
+        public string Subject { get; private set; }
+        public string Message { get; private set; }
+        public ClientEmailNotification(string origin, string destination, string subject, string message)
+        {
+            Origin = origin;
+            Destination = destination;
+            Subject = subject;
+            Message = message;
+        }
+    }
+}
