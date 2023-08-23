054eff9be8483fd77483e511ef485c7608673ed3
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-22 22:06:52 -0300

    construtor clientservice

--- xUnitSoftwareTests/Features/Clients/ClientService.cs
+++ xUnitSoftwareTests/Features/Clients/ClientService.cs
@@ -1,10 +1,20 @@
-﻿namespace Features.Clients
+﻿using MediatR;
+
+namespace Features.Clients
 {
     public class ClientService : IClientService
     {
+        private readonly IClientRepository _clientRepository;
+        private readonly IMediator _mediator;
+
+        public ClientService(IClientRepository clientRepository, IMediator mediator)
+        {
+            _clientRepository = clientRepository;
+            _mediator = mediator;
+        }
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _clientRepository?.Dispose();
         }
     }
 }
