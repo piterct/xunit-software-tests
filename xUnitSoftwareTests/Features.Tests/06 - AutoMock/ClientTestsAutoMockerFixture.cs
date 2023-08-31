2f3a85436246920d07e0e82bf5b2e7883f5e3538
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-30 23:17:57 -0300

    ClientTestsAutoMockerFixture class

---
+++ xUnitSoftwareTests/Features.Tests/06 - AutoMock/ClientTestsAutoMockerFixture.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Features.Tests._06___AutoMock
+{
+    [CollectionDefinition(nameof(ClientAutoMockerColletion))]
+    public class ClientAutoMockerColletion : ICollectionFixture<ClientTestsAutoMockerFixture>
+    { }
+
+    public class ClientTestsAutoMockerFixture : IDisposable
+    {
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
