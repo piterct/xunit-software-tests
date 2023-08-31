0b43ee549796f54e7106eec6049cfbed6e8795d0
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-30 22:58:50 -0300

    Add Client Successful Tests Successful

---
+++ xUnitSoftwareTests/Features.Tests/06 - AutoMock/ClientServiceAutoMockerTests.cs
@@ -0,0 +1,37 @@
+﻿using Features.Clients;
+using MediatR;
+using Moq;
+using Moq.AutoMock;
+using Xunit;
+
+namespace Features.Tests.AutoMock
+{
+    [Collection(nameof(ClientBogusCollection))]
+    public class ClientServiceAutoMockerTests
+    {
+        private readonly ClientBogusTestsFixture _clientBogusTestsFixture;
+        public ClientServiceAutoMockerTests(ClientBogusTestsFixture clientBogusTestsFixture)
+        {
+            _clientBogusTestsFixture = clientBogusTestsFixture;
+        }
+
+
+        [Fact(DisplayName = "Add Client Successful")]
+        [Trait("Category", "Client Service AutoMock Tests")]
+        public void ClientService_Add_MustExecuteSuccessful()
+        {
+            // Arrange
+            var client = _clientBogusTestsFixture.GenerateValidNewClient();
+            var mocker = new AutoMocker();
+            var clientService = mocker.CreateInstance<ClientService>(); 
+
+            //Act
+            clientService.Add(client);
+
+            // Assert
+            Assert.True(client.IsValid());
+            mocker.GetMock<IClientRepository>().Verify(r => r.Add(client), Times.Once);
+            mocker.GetMock<IMediator>().Verify(v => v.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Once);
+        }
+    }
+}
