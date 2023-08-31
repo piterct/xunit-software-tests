4a8787fb564278076f0da7194dfd0a50a325f2c9
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-30 23:19:47 -0300

    GenerateClients ClientAutoMockerFixture

--- xUnitSoftwareTests/Features.Tests/06 - AutoMock/ClientTestsAutoMockerFixture.cs
+++ xUnitSoftwareTests/Features.Tests/06 - AutoMock/ClientTestsAutoMockerFixture.cs
@@ -1,6 +1,9 @@
-﻿using Xunit;
+﻿using Bogus;
+using Bogus.DataSets;
+using Features.Clients;
+using Xunit;
 
-namespace Features.Tests._06___AutoMock
+namespace Features.Tests
 {
     [CollectionDefinition(nameof(ClientAutoMockerColletion))]
     public class ClientAutoMockerColletion : ICollectionFixture<ClientTestsAutoMockerFixture>
@@ -8,7 +11,31 @@ namespace Features.Tests._06___AutoMock
 
     public class ClientTestsAutoMockerFixture : IDisposable
     {
+        public Client GenerateValidNewClient()
+        {
+            return GenerateClients(1, true).FirstOrDefault();
+        }
 
+        public IEnumerable<Client> GenerateClients(int quantity, bool active)
+        {
+
+            var gender = new Faker().PickRandom<Name.Gender>();
+
+            var clients = new Faker<Client>("pt_BR")
+                .CustomInstantiator(f => new Client(
+                    Guid.NewGuid(),
+                    f.Name.FirstName(gender),
+                    f.Name.LastName(gender),
+                    f.Date.Past(80, DateTime.Now.AddDays(-18)),
+                    "",
+                    active,
+                    DateTime.Now))
+                .RuleFor(c => c.Email, (f, c) =>
+                    f.Internet.Email(c.Name.ToLower(), c.LasName.ToLower()));
+
+
+            return clients.Generate(quantity);
+        }
 
         public void Dispose()
         {
