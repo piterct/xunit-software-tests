e720db3e1648fea331842a11dd7d6701c9ea1f69
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-09-02 19:40:11 -0300

    Put new Fact Method to new valid client

---
+++ xUnitSoftwareTests/Features.Tests/07 - FluentAssertions/ClientFluentAssertionsTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClientAutoMockerColletion))]
+    public class ClientFluentAssertionsTests
+    {
+        private readonly ClientTestsAutoMockerFixture _clientTestsAutoMockerFixture;
+
+        public ClientFluentAssertionsTests(ClientTestsAutoMockerFixture clientTestsAutoMockerFixture)
+        {
+            _clientTestsAutoMockerFixture = clientTestsAutoMockerFixture;   
+        }
+
+        [Fact(DisplayName = "New valid client")]
+        [Trait("Category", "Client Fluent Assertion Tests")]
+        public void Client_NewClient_MustBeValid()
+        {
+            // Arrange
+            var client = _clientTestsAutoMockerFixture.GenerateValidNewClient();
+
+            // Act
+            var result = client.IsValid();
+
+            // Assert 
+            Assert.True(result);
+            Assert.Equal(0, client.ValidationResult.Errors.Count);
+        }
+    }
+}
