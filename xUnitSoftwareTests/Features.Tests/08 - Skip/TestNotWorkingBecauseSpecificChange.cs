44df5553973d7e739d10cd9a9ba55b53dd8af269
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-09-09 11:01:58 -0300

    TestNotWorkingBecauseSpecificChange lcass

---
+++ xUnitSoftwareTests/Features.Tests/08 - Skip/TestNotWorkingBecauseSpecificChange.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Features.Tests
+{
+    public class TestNotWorkingBecauseSpecificChange
+    {
+        [Fact(DisplayName = "New Client 2.0", Skip = "New version 2.0 not working")]
+        [Trait("Category", "Skipping Tests")]
+        public void Test_NotWorking_InCompatibleVersion()
+        {
+          Assert.True(false);
+        }
+    }
+}
