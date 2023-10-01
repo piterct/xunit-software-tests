33539d4053c414edafefff8c7f084caf0c9761ac
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-10-01 16:52:14 -0300

    OrderTests - Add Item New Order

---
+++ xUnitSoftwareTests/NerdStore.Sales.Domain.Tests/OrderTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace NerdStore.Sales.Domain.Tests
+{
+    public class OrderTests
+    {
+
+        [Fact(DisplayName = "Add Item New Order ")]
+        [Trait("Category", "Order Tests")]
+        public void AddItemOrder__NewOrder_MustUpdateValue()
+        {
+            // Arrange
+            var order = new Order();
+            var itemOrder = new ItemOrder(Guid.NewGuid(), "Test Product", 2, 100);
+
+            // Act
+            order.Adicionar(itemOrder);
+
+            // Assert
+            Assert.Equal(200, order.TotalValue);
+        }
+    }
+}
