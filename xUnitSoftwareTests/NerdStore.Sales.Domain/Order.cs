c2aee79e89ea2c58d6711643f568d0fa3923fa8e
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-10-01 20:36:30 -0300

    OrderFactory class

--- xUnitSoftwareTests/NerdStore.Sales.Domain/Order.cs
+++ xUnitSoftwareTests/NerdStore.Sales.Domain/Order.cs
@@ -2,17 +2,56 @@
 {
     public class Order
     {
-        public Order()
+        protected Order()
         {
-            _itemsOrder = new List<ItemOrder>();
+            _orderItems = new List<OrderItem>();
         }
+
+        public Guid ClientId { get; private set; }
+
         public decimal TotalValue { get; private set; }
-        public List<ItemOrder> _itemsOrder { get; private set; }
-        public IReadOnlyCollection<ItemOrder> ItemsOrder => _itemsOrder;
-        public void AddItem(ItemOrder itemOrder)
+        public EOrderStatus OrderStatus { get; private set; }
+        public List<OrderItem> _orderItems { get; private set; }
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        public void CalculateValueOrder()
         {
-            _itemsOrder.Add(itemOrder);
-            TotalValue = ItemsOrder.Sum(o => o.Quantity * o.UnitaryValue);
+            TotalValue = OrderItems.Sum(o => o.CalculateValue());
         }
+
+        public void AddItem(OrderItem itemOrder)
+        {
+            if (_orderItems.Any(p => p.ProductId == itemOrder.ProductId))
+            {
+                var existingItem = _orderItems.FirstOrDefault(p => p.ProductId == itemOrder.ProductId);
+                existingItem.AddUnit(itemOrder.Quantity);
+                itemOrder = existingItem;
+
+                _orderItems.Remove(existingItem);
+            }
+
+            _orderItems.Add(itemOrder);
+            CalculateValueOrder();
+        }
+
+        public void SetDraft()
+        {
+            OrderStatus = EOrderStatus.Draft;
+        }
+
+        public static class OrderFactory
+        {
+            public static Order NewOrderDraft(Guid clientId)
+            {
+                var order = new Order
+                {
+                    ClientId = clientId,
+                };
+
+                order.SetDraft();
+                return order;
+            }
+        }
+
     }
 }
