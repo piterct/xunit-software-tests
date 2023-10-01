163ca65ac0404bcbf45fb27df471787839f16102
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-09-30 22:55:16 -0300

    validate invalid cpfs tests

--- xUnitSoftwareTests/Features.Tests/09 - Code Coverage/CpfValidationTests.cs
+++ xUnitSoftwareTests/Features.Tests/09 - Code Coverage/CpfValidationTests.cs
@@ -21,5 +21,24 @@ namespace Features.Tests
             // Act & Assert
             cpfValidation.IsValid(cpf).Should().BeTrue();
         }
+
+
+        [Theory(DisplayName = "CPF Invalidos")]
+        [Trait("Category", "CPF validation Tests")]
+        [InlineData("15231766607213")]
+        [InlineData("528781682082")]
+        [InlineData("35555868512")]
+        [InlineData("36014132822")]
+        [InlineData("72186126500")]
+        [InlineData("23775274811")]
+        public void Cpf_ValidateMultiplesNumbers_AllMustBeInValid(string cpf)
+        {
+            // Assert
+            var cpfValidation = new CpfValidation();
+
+            // Act & Assert
+            cpfValidation.IsValid(cpf).Should().BeFalse();
+        }
     }
+}
 }
