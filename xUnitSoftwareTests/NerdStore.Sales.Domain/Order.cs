8285568baf71259e954f0a737ca03ceebb656d18
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-10-01 17:20:11 -0300

    Order Class

--- xUnitSoftwareTests/NerdStore.Sales.Domain/Order.cs
+++ xUnitSoftwareTests/NerdStore.Sales.Domain/Order.cs
@@ -2,5 +2,17 @@
 {
     public class Order
     {
+        public Order()
+        {
+            _itemsOrder = new List<ItemOrder>();
+        }
+        public decimal TotalValue { get; private set; }
+        public List<ItemOrder> _itemsOrder { get; private set; }
+        public IReadOnlyCollection<ItemOrder> ItemsOrder => _itemsOrder;
+        public void AddItem(ItemOrder itemOrder)
+        {
+            _itemsOrder.Add(itemOrder);
+            TotalValue = ItemsOrder.Sum(o => o.Quantity * o.UnitaryValue);
+        }
     }
 }
