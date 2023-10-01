64aa805856c9c7aa44fbf938af30afa45fde3e73
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-10-01 20:28:53 -0300

    Add Order Item -  Existing Order Item - Tests

--- xUnitSoftwareTests/NerdStore.Sales.Domain.Tests/OrderTests.cs
+++ xUnitSoftwareTests/NerdStore.Sales.Domain.Tests/OrderTests.cs
@@ -10,11 +10,11 @@ namespace NerdStore.Sales.Domain.Tests
         public void AddOrderItem__NewOrder_MustUpdateValue()
         {
             // Arrange
-            var order = new Order();
-            var itemOrder = new OrderItem(Guid.NewGuid(), "Test Product", 2, 100);
+            var order = Order.OrderFactory.NewOrderDraft(Guid.NewGuid());
+            var orderItem = new OrderItem(Guid.NewGuid(), "Test Product", 2, 100);
 
             // Act
-            order.AddItem(itemOrder);
+            order.AddItem(orderItem);
 
             // Assert
             Assert.Equal(200, order.TotalValue);
@@ -25,11 +25,20 @@ namespace NerdStore.Sales.Domain.Tests
         public void AddOrderItem__ExistingOrderItem_MustIncrementItemsAndSumValues()
         {
             // Arrange
-           
+            var order = Order.OrderFactory.NewOrderDraft(Guid.NewGuid());
+            var productId = Guid.NewGuid();
+            var orderItem = new OrderItem(productId, "Test Product", 2, 100);
+            order.AddItem(orderItem);
+
+            var orderItem2 = new OrderItem(productId, "Test Product", 1, 100);
 
             // Act
+            order.AddItem(orderItem2);
 
             // Assert
+            Assert.Equal(300, order.TotalValue);
+            Assert.Equal(1,order.OrderItems.Count);
+            Assert.Equal(3, order.OrderItems.FirstOrDefault(P=> P.ProductId == productId).Quantity);
         }
     }
 }
