e59d4c195d0ce28e7e4033993d204474b23ea158
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-09-30 22:44:48 -0300

    CpfValidationTests  validate Multiples Numbers Valids

---
+++ xUnitSoftwareTests/Features.Tests/09 - Code Coverage/CpfValidationTests.cs
@@ -0,0 +1,25 @@
+﻿using Features.Core;
+using FluentAssertions;
+using Xunit;
+
+namespace Features.Tests
+{
+    public class CpfValidationTests
+    {
+        [Theory(DisplayName = "Valids Cpfs")]
+        [Trait("Category", "CPF validation Tests")]
+        [InlineData("15231766607")]
+        [InlineData("78246847333")]
+        [InlineData("64184957307")]
+        [InlineData("21681764423")]
+        [InlineData("13830803800")]
+        public void Cpf_ValidateMultiplesNumbers_AllMustBeValid(string cpf)
+        {
+            // Assert
+            var cpfValidation = new CpfValidation();
+
+            // Act & Assert
+            cpfValidation.IsValid(cpf).Should().BeTrue();
+        }
+    }
+}
