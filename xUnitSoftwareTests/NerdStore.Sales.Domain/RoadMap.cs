fe99ed44a3eb32065c526a913609156c88783595
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-10-01 16:16:00 -0300

    RoadMap - 4 Apply discount voucher

--- xUnitSoftwareTests/NerdStore.Sales.Domain/RoadMap.cs
+++ xUnitSoftwareTests/NerdStore.Sales.Domain/RoadMap.cs
@@ -27,6 +27,23 @@
             3 - Item Removal
                 3.1 - The item must be on the list to be removed
                 3.2 - When removing an item it is necessary to calculate the total order value
+
+            A voucher has a unique code and the discount can be in percentage or fixed value
+            Use a flag indicating that an order had a discount voucher applied and the amount of the discount generated
+
+            4 - Apply discount voucher 
+                4.1 - The voucher can only be applied if it is valid, for this reason:
+                    4.1.1 - Must have a code
+                    4.1.2 - The expiration date is greater than the current date
+                    4.1.3 - The voucher is active
+                    4.1.4 - The voucher possess a available quantity
+                    4.1.5 - One of the discount forms must be filled in with a value above 0
+                4.2 - Calculate the discount depending on the voucher type
+                    4.2.1 - Voucher with percentage discount
+                    4.2.2 - Voucher with discount in (real) values
+                4.3 - When the discount value exceeds the order total, the order receives the value: 0
+                4.4 - After applying the voucher, the discount must be re-calculated after any modification
+                      to the list of order items
         */
     }
 }
