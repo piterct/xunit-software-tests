a77a6fdfd3ccb85248663e18378f3065715023e0
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-10-01 16:59:54 -0300

    ItemOrder Class

---
+++ xUnitSoftwareTests/NerdStore.Sales.Domain/ItemOrder.cs
@@ -0,0 +1,18 @@
+﻿namespace NerdStore.Sales.Domain
+{
+    public class ItemOrder
+    {
+        public ItemOrder(Guid productId, string productName, int quantity, decimal unitaryValue)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Quantity = quantity;
+            UnitaryValue = unitaryValue;
+        }
+
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitaryValue { get; set; }
+    }
+}
