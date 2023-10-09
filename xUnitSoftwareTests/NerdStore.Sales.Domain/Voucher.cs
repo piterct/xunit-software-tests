f66e17674c883afe8ec0aa2d28252ef2b1ed105a
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-10-08 22:32:22 -0300

    AbstractValidator - Messages

--- xUnitSoftwareTests/NerdStore.Sales.Domain/Voucher.cs
+++ xUnitSoftwareTests/NerdStore.Sales.Domain/Voucher.cs
@@ -1,29 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
 
 namespace NerdStore.Sales.Domain
 {
     public class Voucher
     {
-        //public Voucher(string code, decimal discountValue, decimal discountPercentage, DateTime? expirationDate, bool active, bool used)
-        //{
-        //    Code = code;
-        //    DiscountValue = discountValue;
-        //    DiscountPercentage = discountPercentage;
-        //    ExpirationDate = expirationDate;
-        //    Active = active;
-        //    Used = used;
-        //}
+        public Voucher(string code, decimal? discountPercentage, decimal? discountValue,int quantity, ETypeOfDiscountVoucher typeOfDiscountVoucher, DateTime expirationDate, bool active, bool used)
+        {
+            Code = code;
+            DiscountPercentage = discountPercentage;
+            DiscountValue = discountValue;
+            Quantity = quantity;
+            TypeOfDiscountVoucher = typeOfDiscountVoucher;
+            ExpirationDate = expirationDate;
+            Active = active;
+            Used = used;
+        }
 
-        public string Code { get; set; }
-        public decimal DiscountValue { get; set; }
-        public decimal DiscountPercentage { get; set; }
-        public DateTime? ExpirationDate { get; set;}
-        public bool Active { get; set; }
-        public bool Used { get; set; }
+        public string Code { get; private set; }
+        public decimal? DiscountPercentage { get; private set; }
+        public decimal? DiscountValue { get; private set; }
+        public int Quantity { get; private set; }
+        public ETypeOfDiscountVoucher TypeOfDiscountVoucher { get; private set; }
+        public DateTime ExpirationDate { get; private set;}
+        public bool Active { get; private set; }
+        public bool Used { get; private set; }
+
+        public bool ValidateIfIsApplicable()
+        {
+            return true;
+        }
+
+        public class VoucherApplicableValidation : AbstractValidator<Voucher>
+        {
+            public static string VoucherWihoutValidCode => "Voucher without valid code.";
+            public static string ExpiredVoucher => "This voucher is expired.";
+            public static string NoLongerValidVoucher => "This voucher is no longer valid.";
+            public static string VoucherAlreadyUsed => "This voucher has already been used.";
+            public static string NoLongerAvaibleVoucher => "This voucher is no longer available";
+            public static string ValueGreaterThanZero => "The discount value must be greater than 0";
+            public static string PercentageValueGreaterThanZero => "The discount percentage value must be greater than 0";
+        }
 
     }
 }
