a1a222185ff6c979f580d1272ae45b2ba4f48187
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-10-08 21:59:54 -0300

    Voucher_ValidateATypeValueVoucherValida_MustBeValid -  method

--- xUnitSoftwareTests/NerdStore.Sales.Domain.Tests/VoucherTests.cs
+++ xUnitSoftwareTests/NerdStore.Sales.Domain.Tests/VoucherTests.cs
@@ -1,6 +1,19 @@
-﻿namespace NerdStore.Sales.Domain.Tests
+﻿using Xunit;
+
+namespace NerdStore.Sales.Domain.Tests
 {
     public class VoucherTests
     {
+        [Fact(DisplayName = "Validate a type value valid voucher ")]
+        [Trait("Category", "Sales - Voucher")]
+        public void Voucher_ValidateATypeValueVoucherValida_MustBeValid()
+        {
+            // Arrange
+            var order = Order.OrderFactory.NewOrderDraft(Guid.NewGuid());
+
+
+
+           
+        }
     }
 }
