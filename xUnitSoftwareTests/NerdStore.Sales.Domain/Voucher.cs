d6196b9fb5610c846cd87b250ad2f70630dc2fb0
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-10-08 22:43:50 -0300

    ExpirationGreaterThanCurrentl and AbstractValidator constructor

--- xUnitSoftwareTests/NerdStore.Sales.Domain/Voucher.cs
+++ xUnitSoftwareTests/NerdStore.Sales.Domain/Voucher.cs
@@ -4,7 +4,7 @@ namespace NerdStore.Sales.Domain
 {
     public class Voucher
     {
-        public Voucher(string code, decimal? discountPercentage, decimal? discountValue,int quantity, ETypeOfDiscountVoucher typeOfDiscountVoucher, DateTime expirationDate, bool active, bool used)
+        public Voucher(string code, decimal? discountPercentage, decimal? discountValue, int quantity, ETypeOfDiscountVoucher typeOfDiscountVoucher, DateTime expirationDate, bool active, bool used)
         {
             Code = code;
             DiscountPercentage = discountPercentage;
@@ -21,7 +21,7 @@ namespace NerdStore.Sales.Domain
         public decimal? DiscountValue { get; private set; }
         public int Quantity { get; private set; }
         public ETypeOfDiscountVoucher TypeOfDiscountVoucher { get; private set; }
-        public DateTime ExpirationDate { get; private set;}
+        public DateTime ExpirationDate { get; private set; }
         public bool Active { get; private set; }
         public bool Used { get; private set; }
 
@@ -32,6 +32,7 @@ namespace NerdStore.Sales.Domain
 
         public class VoucherApplicableValidation : AbstractValidator<Voucher>
         {
+
             public static string VoucherWihoutValidCode => "Voucher without valid code.";
             public static string ExpiredVoucher => "This voucher is expired.";
             public static string NoLongerValidVoucher => "This voucher is no longer valid.";
@@ -39,6 +40,53 @@ namespace NerdStore.Sales.Domain
             public static string NoLongerAvaibleVoucher => "This voucher is no longer available";
             public static string ValueGreaterThanZero => "The discount value must be greater than 0";
             public static string PercentageValueGreaterThanZero => "The discount percentage value must be greater than 0";
+
+            public VoucherApplicableValidation()
+            {
+                RuleFor(c => c.Code)
+                    .NotEmpty()
+                    .WithMessage(VoucherWihoutValidCode);
+
+                RuleFor(c => c.ExpirationDate)
+                    .Must(ExpirationGreaterThanCurrentl)
+                    .WithMessage(ExpiredVoucher);
+
+                RuleFor(c => c.Active)
+                    .Equal(true)
+                    .WithMessage(NoLongerValidVoucher);
+
+                RuleFor(c => c.Used)
+                    .Equal(false)
+                    .WithMessage(VoucherAlreadyUsed);
+
+                RuleFor(c => c.Quantity)
+                    .GreaterThan(0)
+                    .WithMessage(NoLongerAvaibleVoucher);
+
+                When(f => f.TypeOfDiscountVoucher == ETypeOfDiscountVoucher.Value, () =>
+                {
+                    RuleFor(f => f.DiscountValue)
+                        .NotNull()
+                        .WithMessage(ValueGreaterThanZero)
+                        .GreaterThan(0)
+                        .WithMessage(ValueGreaterThanZero);
+                });
+
+                When(f => f.TypeOfDiscountVoucher == ETypeOfDiscountVoucher.Percentage, () =>
+                {
+                    RuleFor(f => f.DiscountPercentage)
+                        .NotNull()
+                        .WithMessage(PercentageValueGreaterThanZero)
+                        .GreaterThan(0)
+                        .WithMessage(PercentageValueGreaterThanZero);
+                });
+            }
+
+            protected static bool ExpirationGreaterThanCurrentl(DateTime expirationDate)
+            {
+                return expirationDate >= DateTime.Now;
+            }
+
         }
 
     }
