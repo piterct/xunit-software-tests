b318a7e997504a7a77ce1d936123bf7d0930093b
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-11-15 22:18:02 -0300

    AddItemOrderCommandTests

---
+++ xUnitSoftwareTests/NerdStore.Sales.Application.Tests/Commands/Order/AddItemOrderCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace NerdStore.Sales.Application.Tests.Commands.Order
+{
+    public class AddItemOrderCommandTests
+    {
+        [Fact(DisplayName = "Add Item valid command ")]
+        [Trait("Category", "Sales - Order Commands")]
+        public void AddItemOrderCommand__CommandMustBeValid__MustWorkAtValidation()
+        {
+            // Arrange
+            var oderItemCommand = new AddItemOrderCommand(Guid.NewGuid(), Guid.NewGuid(), "Test Product", 2, 100);
+
+            // Act
+            var result = oderItemCommand.isValid();
+
+            // Assert
+            Assert.True(result);
+
+        }
+    }
+}
