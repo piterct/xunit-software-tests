9a6b52e7a0b301aff253b9a0e9ef48689b8951a5
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-11-15 22:29:43 -0300

    AddItemOrderCommand

---
+++ xUnitSoftwareTests/NerdStore.Sales.Application/Commands/AddItemOrderCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace NerdStore.Sales.Application.Commands
+{
+    public class AddItemOrderCommand
+    {
+        public Guid ClientId { get; set; }
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitValue { get; set; }
+
+        public AddItemOrderCommand()
+        {
+
+        }
+
+    }
+}
