3522350b662e7344756720ef5990199815a2a266
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-11-16 22:39:12 -0300

    OrderCommandHandle with orderRepository

--- xUnitSoftwareTests/NerdStore.Sales.Application/Commands/OrderCommandHandler.cs
+++ xUnitSoftwareTests/NerdStore.Sales.Application/Commands/OrderCommandHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace NerdStore.Sales.Application.Commands
+﻿using NerdStore.Sales.Domain;
+using NerdStore.Sales.Domain.Repository;
+
+namespace NerdStore.Sales.Application.Commands
 {
     public class OrderCommandHandler
     {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderCommandHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public bool Handle(AddItemOrderCommand message)
+        {
+            _orderRepository.Add(Order.OrderFactory.NewOrderDraft(message.ClientId));
+            return true;
+        }
     }
 }
