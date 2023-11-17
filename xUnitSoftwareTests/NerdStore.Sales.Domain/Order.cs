d59e4c77d715c8ebddda1365808d5c68afb5f4e3
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-11-17 00:25:08 -0300

    Order Class inherit Entity

--- xUnitSoftwareTests/NerdStore.Sales.Domain/Order.cs
+++ xUnitSoftwareTests/NerdStore.Sales.Domain/Order.cs
@@ -3,7 +3,7 @@ using NerdStore.Core.DomainObjects;
 
 namespace NerdStore.Sales.Domain
 {
-    public class Order
+    public class Order : Entity
     {
         public static int MAX_UNITS_ITEM => 15;
         public static int MIN_UNITS_ITEM => 1;
