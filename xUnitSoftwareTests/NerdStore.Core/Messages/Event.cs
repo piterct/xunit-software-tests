1d423b52e4f936e57aed90d3d69351b27016ace4
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-11-17 00:15:46 -0300

    Event class Notification

---
+++ xUnitSoftwareTests/NerdStore.Core/Messages/Event.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace NerdStore.Core.Messages
+{
+    public abstract class Event : Message, INotification
+    {
+        public DateTime Timestamp { get; set; }
+
+        protected Event()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
