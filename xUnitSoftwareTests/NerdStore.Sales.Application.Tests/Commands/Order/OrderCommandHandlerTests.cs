b6c7d239a2632ec265dbb1ae1b3557ca46b28da0
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-11-16 21:53:17 -0300

    Add new item order successful Tests

--- xUnitSoftwareTests/NerdStore.Sales.Application.Tests/Commands/Order/OrderCommandHandlerTests.cs
+++ xUnitSoftwareTests/NerdStore.Sales.Application.Tests/Commands/Order/OrderCommandHandlerTests.cs
@@ -1,6 +1,24 @@
-﻿namespace NerdStore.Sales.Application.Tests.Commands.Order
+﻿using NerdStore.Sales.Application.Commands;
+using Xunit;
+
+namespace NerdStore.Sales.Application.Tests.Commands.Order
 {
     public class OrderCommandHandlerTests
     {
+
+        [Fact(DisplayName = "Add new item order successful")]
+        [Trait("Category", "Sales - Order Command Handler")]
+        public void AddItem__NewOrder__MustExecuteSuccessful()
+        {
+            // Arrange
+            
+
+            // Act
+        
+
+            // Assert
+           
+
+        }
     }
 }
