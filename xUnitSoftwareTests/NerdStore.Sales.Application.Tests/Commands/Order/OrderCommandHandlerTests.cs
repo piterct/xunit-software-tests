3fa2fb996c494db75393f239491cf19a8fe0ff96
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-11-16 22:04:34 -0300

    Class OrderCommandHandler

--- xUnitSoftwareTests/NerdStore.Sales.Application.Tests/Commands/Order/OrderCommandHandlerTests.cs
+++ xUnitSoftwareTests/NerdStore.Sales.Application.Tests/Commands/Order/OrderCommandHandlerTests.cs
@@ -1,4 +1,6 @@
-﻿using NerdStore.Sales.Application.Commands;
+﻿using Moq;
+using Moq.AutoMock;
+using NerdStore.Sales.Application.Commands;
 using Xunit;
 
 namespace NerdStore.Sales.Application.Tests.Commands.Order
@@ -11,13 +13,18 @@ namespace NerdStore.Sales.Application.Tests.Commands.Order
         public void AddItem__NewOrder__MustExecuteSuccessful()
         {
             // Arrange
-            
+            var orderCommand = new AddItemOrderCommand(Guid.NewGuid(), Guid.NewGuid(), "Test Product", 2, 100);
 
-            // Act
-        
+            var mocker = new AutoMocker();
+            var orderHandler = mocker.CreateInstance<OrderCommandHandler>();
 
             // Assert
-           
+            var result = orderHandler.Handler(orderCommand);
+
+            //Assert
+            Assert.True(result);
+            mocker.GetMock(IOrderRepository)().Verify(r => r.Add(It.IsAny<Domain.Order>()), Times.Once());
+            mocker.GetMock(IMediator)().Verify(r => r.Publish(It.IsAny<INotification>()), Times.Once());
 
         }
     }
