c7c9e93ae279877b963b479e1d24b8e2dbdcbcba
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-11-17 00:15:28 -0300

    AddedOrderItemEvent with constructor

--- xUnitSoftwareTests/NerdStore.Sales.Application/Events/AddedOrderItemEvent.cs
+++ xUnitSoftwareTests/NerdStore.Sales.Application/Events/AddedOrderItemEvent.cs
@@ -1,8 +1,25 @@
-﻿using MediatR;
+﻿using NerdStore.Core.Messages;
 
 namespace NerdStore.Sales.Application.Events
 {
-    public class AddedOrderItemEvent : INotification
+    public class AddedOrderItemEvent : Event
     {
+        public Guid ClientId { get; private set; }
+        public Guid OrderId { get; private set; }
+        public Guid ProductId { get; private set; }
+        public string NameProduct { get; set; }
+        public decimal UnitValue { get; private set; }
+        public int Quantity { get; private set; }
+
+        public AddedOrderItemEvent(Guid clientId, Guid orderId, Guid productId, string nameProduct, decimal unitValue, int quantity)
+        {
+                ClientId = clientId;
+                OrderId = orderId;
+                ProductId = productId;
+                NameProduct = nameProduct;
+                UnitValue = unitValue;
+                Quantity = quantity;
+
+        }
     }
 }
