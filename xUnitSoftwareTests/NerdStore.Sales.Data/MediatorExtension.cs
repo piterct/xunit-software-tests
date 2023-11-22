31408183660b7b4b5ad7646339fb097a49893afe
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-11-22 01:00:43 -0300

    MediatorExtension class

---
+++ xUnitSoftwareTests/NerdStore.Sales.Data/MediatorExtension.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Sales.Data
+{
+    public static class MediatorExtension
+    {
+        public static async Task PublishEvents(this IMediator mediator, SalesContext ctx)
+        {
+            var domainEntities = ctx.ChangeTracker
+                .Entries<Entity>()
+                .Where(x => x.Entity.Notifications != null && x.Entity.Notifications.Any());
+
+            var domainEvents = domainEntities
+                .SelectMany(x => x.Entity.Notifications)
+                .ToList();
+
+            domainEntities.ToList()
+                .ForEach(entity => entity.Entity.ClearEvents());
+
+            var tasks = domainEvents
+                .Select(async (domainEvent) => {
+                    await mediator.Publish(domainEvent);
+                });
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
