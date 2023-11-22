6ccf09365ca4973ff47351044755bbb5ab0c37e1
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-11-22 00:35:00 -0300

    AddEvent

--- xUnitSoftwareTests/NerdStore.Core/DomainObjects/Entity.cs
+++ xUnitSoftwareTests/NerdStore.Core/DomainObjects/Entity.cs
@@ -1,12 +1,23 @@
-﻿namespace NerdStore.Core.DomainObjects
+﻿using NerdStore.Core.Messages;
+
+namespace NerdStore.Core.DomainObjects
 {
     public abstract class Entity
     {
         public Guid Id { get; set; }
+        private List<Event> _notifications;
+        public IReadOnlyCollection<Event> Notifications => _notifications?.AsReadOnly();
 
         protected Entity()
         {
             Id = Guid.NewGuid();
         }
+
+        public void AddEvent(Event eventValue)
+        {
+            _notifications = _notifications ?? new List<Event>();
+            _notifications.Add(eventValue);
+        }
+
     }
 }
