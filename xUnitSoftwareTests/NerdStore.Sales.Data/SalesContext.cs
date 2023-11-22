d5818022304a3b5d36e80beb1ad88f9cf1994476
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-11-22 01:01:19 -0300

    Sales Context

---
+++ xUnitSoftwareTests/NerdStore.Sales.Data/SalesContext.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NerdStore.Core.Data;
+using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Sales.Data
+{
+    public class SalesContext : DbContext, IUnitOfWork
+    {
+        private readonly IMediator _mediator;
+
+        public SalesContext(DbContextOptions<SalesContext> options, IMediator mediator) : base(options)
+        {
+            _mediator = mediator;
+        }
+        public async Task<bool> Commit()
+        {
+            var success = await base.SaveChangesAsync() > 0;
+            if (success) await _mediator.PublishEvents(this);
+
+            return success;
+
+        }
+    }
+
+}
