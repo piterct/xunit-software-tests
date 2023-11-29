650433181f5276f3afdea1d5115b87f3459783d9
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-11-29 01:28:00 -0300

    Put class DomainNotification

--- xUnitSoftwareTests/NerdStore.Sales.Application/Commands/OrderCommandHandler.cs
+++ xUnitSoftwareTests/NerdStore.Sales.Application/Commands/OrderCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using NerdStore.Core.DomainObjects;
 using NerdStore.Sales.Application.Events;
 using NerdStore.Sales.Domain;
 using NerdStore.Sales.Domain.Repository;
@@ -19,6 +20,16 @@ namespace NerdStore.Sales.Application.Commands
 
         public async Task<bool> Handle(AddItemOrderCommand message, CancellationToken cancellationToken)
         {
+            if (!message.IsValid())
+            {
+                foreach(var error in message.ValidationResult.Errors)
+                {
+                    await _mediator.Publish(new DomainNotification(message.MessageType, error.ErrorMessage), cancellationToken);
+                }
+
+                return false;
+            }
+
             var order = await _orderRepository.GetDraftOrderByClientId(message.ClientId);
             var orderItem = new OrderItem(message.ProductId, message.Name, message.Quantity, message.UnitValue);
 
