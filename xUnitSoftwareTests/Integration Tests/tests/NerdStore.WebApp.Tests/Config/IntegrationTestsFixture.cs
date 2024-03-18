0fcca5dba26c293b161c3e31d0a88239dbfd5f4a
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-18 00:22:55 -0300

    IntegrationTestsFixture added

---
+++ xUnitSoftwareTests/Integration Tests/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixture.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using NerdStore.WebApp.MVC;
+
+namespace NerdStore.WebApp.Tests.Config
+{
+    [CollectionDefinition(nameof(IntegrationTestsFixtureCollection))]
+
+    public class IntegrationTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupWebTests>> { }
+    [CollectionDefinition(nameof(IntegrationApiTestsFixtureCollection))]
+    public class IntegrationApiTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupApiTests>> { }
+    public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
+    {
+        public readonly LojaAppFactory<TStartup> Factory;
+        public HttpClient Client;
+
+        public IntegrationTestsFixture()
+        {
+            var clientOptions = new WebApplicationFactoryClientOptions
+            {
+
+            };
+
+            Factory = new LojaAppFactory<TStartup>();
+            Client = Factory.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            Factory.Dispose();
+        }
+    }
+}
