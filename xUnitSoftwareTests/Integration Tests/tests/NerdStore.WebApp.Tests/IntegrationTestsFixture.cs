5c9094e675dda4b4401390d166b5f1aa8ac4421a
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-18 00:09:55 -0300

    Colletcion fixture project

---
+++ xUnitSoftwareTests/Integration Tests/tests/NerdStore.WebApp.Tests/IntegrationTestsFixture.cs
@@ -0,0 +1,16 @@
+﻿using NerdStore.WebApp.MVC;
+
+namespace NerdStore.WebApp.Tests
+{
+    [CollectionDefinition(nameof(IntegrationTestsFixtureCollection))]
+
+    public class IntegrationTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupWebTests>> { }
+    [CollectionDefinition(nameof(IntegrationApiTestsFixtureCollection))]
+    public class IntegrationApiTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupApiTests>> { }
+    public class IntegrationTestsFixture<IStartup> : IDisposable where IStartup : class
+    {
+        public void Dispose()
+        {
+        }
+    }
+}
