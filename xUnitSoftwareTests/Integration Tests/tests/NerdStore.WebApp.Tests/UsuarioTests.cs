72ac2e43bc66afe575a4112a91ef9694ae85e76b
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-18 00:22:31 -0300

    User class create to integration the tests with webapplication

---
+++ xUnitSoftwareTests/Integration Tests/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -0,0 +1,15 @@
+﻿using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
+
+namespace NerdStore.WebApp.Tests
+{
+    public class UsuarioTests
+    {
+        private readonly IntegrationTestsFixture<StartupWebTests> _integrationTestsFixture;
+
+        public UsuarioTests(IntegrationTestsFixture<StartupWebTests> integrationTestsFixture)
+        {
+            _integrationTestsFixture = integrationTestsFixture;
+        }
+    }
+}
