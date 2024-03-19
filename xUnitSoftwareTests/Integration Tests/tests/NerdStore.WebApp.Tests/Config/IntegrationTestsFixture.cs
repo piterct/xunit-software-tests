e6bf8288c471c7a1cfe369af6908fba8755a4580
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-18 23:06:31 -0300

    GetAntiForgeryToken method

--- xUnitSoftwareTests/Integration Tests/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixture.cs
+++ xUnitSoftwareTests/Integration Tests/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixture.cs
@@ -1,15 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc.Testing;
 using NerdStore.WebApp.MVC;
 
 namespace NerdStore.WebApp.Tests.Config
 {
-    [CollectionDefinition(nameof(IntegrationTestsFixtureCollection))]
+    [CollectionDefinition(nameof(IntegrationWebTestsFixtureCollection))]
+    public class IntegrationWebTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupWebTests>> { }
 
-    public class IntegrationTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupWebTests>> { }
     [CollectionDefinition(nameof(IntegrationApiTestsFixtureCollection))]
     public class IntegrationApiTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupApiTests>> { }
     public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
     {
+        public string AntiForgeryFieldName = "__RequestVerificationToken";
+
         public readonly ShopAppFactory<TStartup> Factory;
         public HttpClient Client;
 
@@ -22,6 +25,19 @@ namespace NerdStore.WebApp.Tests.Config
 
             Factory = new ShopAppFactory<TStartup>();
             Client = Factory.CreateClient();
+        }
+
+        public string GetAntiForgeryToken(string htmlBody)
+        {
+            var requestVerificationTokenMatch =
+                Regex.Match(htmlBody, $@"\<input name=""{AntiForgeryFieldName}"" type=""hidden"" value=""([^""]+)"" \/\>");
+
+            if (requestVerificationTokenMatch.Success)
+            {
+                return requestVerificationTokenMatch.Groups[1].Captures[0].Value;
+            }
+
+            throw new ArgumentException($"Anti forgery token '{AntiForgeryFieldName}' not found in HTML", nameof(htmlBody));
         }
 
         public void Dispose()
