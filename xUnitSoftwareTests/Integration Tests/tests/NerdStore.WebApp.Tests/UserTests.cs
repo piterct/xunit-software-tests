39875680a0cec6d7ec4d89b90d942eb76f9bcd7a
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-18 22:10:37 -0300

    UserTests class

---
+++ xUnitSoftwareTests/Integration Tests/tests/NerdStore.WebApp.Tests/UserTests.cs
@@ -0,0 +1,22 @@
+﻿using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
+
+namespace NerdStore.WebApp.Tests
+{
+    public class UserTests
+    {
+        private readonly IntegrationTestsFixture<StartupWebTests> _integrationTestsFixture;
+
+        public UserTests(IntegrationTestsFixture<StartupWebTests> integrationTestsFixture)
+        {
+            _integrationTestsFixture = integrationTestsFixture;
+        }
+        [Fact(DisplayName = "Complete registration successfully")]
+        [Trait("Category", "Integration Web - User")]
+        public async Task User_RegistrationUser_MustBeSuccessful()
+        {
+            //Arrange
+            
+        }
+    }
+}
