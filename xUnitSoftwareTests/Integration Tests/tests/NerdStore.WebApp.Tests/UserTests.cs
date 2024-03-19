323673c2daa2e09102736087592f29f2f5f3e673
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-18 22:50:53 -0300

    Input datas inside of the form with integration tests

--- xUnitSoftwareTests/Integration Tests/tests/NerdStore.WebApp.Tests/UserTests.cs
+++ xUnitSoftwareTests/Integration Tests/tests/NerdStore.WebApp.Tests/UserTests.cs
@@ -3,6 +3,7 @@ using NerdStore.WebApp.Tests.Config;
 
 namespace NerdStore.WebApp.Tests
 {
+    [Collection(nameof(IntegrationWebTestsFixtureCollection))]
     public class UserTests
     {
         private readonly IntegrationTestsFixture<StartupWebTests> _integrationTestsFixture;
@@ -16,7 +17,32 @@ namespace NerdStore.WebApp.Tests
         public async Task User_RegistrationUser_MustBeSuccessful()
         {
             //Arrange
-            
+            var initialResponse = await _integrationTestsFixture.Client.GetAsync("/Identity/Account/Register");
+            initialResponse.EnsureSuccessStatusCode();
+
+            var email = "tests@teste.com";
+
+            var formData = new Dictionary<string, string>
+            {
+                {"Input.Email", email},
+                {"Input.Password", "Tests@123"},
+                {"Input.ConfirmPassword", "Tests@123"}
+            };
+
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Register")
+            {
+                Content = new FormUrlEncodedContent(formData)
+            };
+
+            //Act
+            var postResponse = await _integrationTestsFixture.Client.SendAsync(postRequest);
+
+            //Assert
+            var responseString = await postResponse.Content.ReadAsStringAsync();
+
+            postResponse.EnsureSuccessStatusCode();
+            Assert.Contains($"Hello {email}!", responseString);
+
         }
     }
 }
